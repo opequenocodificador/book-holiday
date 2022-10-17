61c951ec928cd67011d48fc34aa4a72c740e8691
Monciaux Sylvain <monciauxsylvain63@gmail.com>
2022-10-17 09:57:29 +0100

    add function to change date

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Convert today to input format
 //Pour entrer dans un input la date doit être au format aaaa mm jj
 const today = new Date().toISOString().split("T")[0];
-
 start_date.value = today;
 
 //Empêcher l'utilisateur de prendre une date antèrieure à la date du jour
@@ -28,13 +27,21 @@ start_date.addEventListener("change", (e) => {
   }
 });
 
-//Changer la date de départ si la date d'arrivée est supérieur à celle-ci
+//*****//Changer la date de départ si la date d'arrivée est infèrieure à celle-ci *//
+end_date.addEventListener("change", (e) => {
+  let dayBefore = new Date(end_date.value);
+  dayBefore = new Date(end_date.value).toISOString().split("T")[0];
+  if (end_date.value < start_date.value) {
+    start_date.value = dayBefore;
+  }
+});
+
+//Calculer le nombre de jour entre l'arrivée et le départ
 const bookingCalc = () => {
   var day1 = new Date(start_date.value);
   var day2 = new Date(end_date.value);
 
   let difference = Math.abs(day2 - day1) / (1000 * 3600 * 24);
-  console.log(difference);
 };
 
 start_date.addEventListener("change", bookingCalc);
